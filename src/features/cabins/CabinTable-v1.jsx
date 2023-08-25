8af86537dfa476470b46ede8639af77fb4882bd1
yaredow <yaredyilma11@gmail.com>
2023-08-25 15:25:20 +0300

    create a reusable table

--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -3,7 +3,6 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "../cabins/CabinRow";
 import useCabin from "./useCabin";
 import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -26,22 +25,19 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Menus>
-      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-        <Table.Header>
-          <div></div>
-          <div>Cabin</div>
-          <div>Capacity</div>
-          <div>Price</div>
-          <div>Dicount</div>
-          <div></div>
-        </Table.Header>
-        <Table.Body
-          data={cabins}
-          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-        />
-      </Table>
-    </Menus>
+    <Table column="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header>
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Dicount</div>
+        <div></div>
+      </Table.Header>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
   );
 }
 
